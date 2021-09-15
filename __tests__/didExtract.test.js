7d74bb8c16463194ebc291842c8dd3b13a79aaef
Ese <silviaox@gmail.com>
2021-09-14 22:09:07 -0300

    add test for getDidContract 522

--- __tests__/didExtract.test.js
+++ __tests__/didExtract.test.js
@@ -1,18 +1,58 @@
 import { BlockchainManager } from "../src/BlockchainManager";
 import { DIDS } from "./constants/Constants";
+import Web3 from "web3";
 const Constants = require("./constants/Constants");
 
+const options={ from: "0x0d0fa2cd3813412e94597103dbf715c7afb8c038"};
 
 describe("BlockchainManager did getters", () => {
+  // describe("getDidContract", () => {
+  //   test.todo("Get ethr-did-registry address for all networks");
+  // });
+  
   describe("getDidContract", () => {
-    test.todo("Get Lacchain ethr-did-registry address");
-    test.todo("Get BFA ethr-did-registry address");
-    test.todo("Get mainnet ethr-did-registry address");
-    test.todo("Get ropsten ethr-did-registry address");
-    test.todo("Get rinkeby ethr-did-registry address");
-    test.todo("Get goerli ethr-did-registry address");
-    test.todo("Get kovan ethr-did-registry address");
-    test.todo("Get ethr-did-registry address for all networks");
+    test("Get Lacchain ethr-did-registry address", async() => {
+      const web3 = new Web3(new Web3.providers.HttpProvider(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[5].rpcUrl));
+      const resul = await BlockchainManager.getDidContract(options,Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[5].registry,web3);
+      expect(resul._address).toBe(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[5].registry);
+    });
+
+    test("Get BFA ethr-did-registry address", async() => {
+      const web3 = new Web3(new Web3.providers.HttpProvider(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[6].rpcUrl));
+      const resul = await BlockchainManager.getDidContract(options,Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[6].registry,web3);
+      expect(resul._address).toBe(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[6].registry);
+    });
+
+    test("Get mainnet ethr-did-registry address", async() => {
+      const web3 = new Web3(new Web3.providers.HttpProvider(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[0].rpcUrl));
+      const resul = await BlockchainManager.getDidContract(options,Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[0].registry,web3);
+      expect(resul._address).toBe(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[0].registry);
+    });
+
+    test("Get ropsten ethr-did-registry address", async() => {
+      const web3 = new Web3(new Web3.providers.HttpProvider(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[1].rpcUrl));
+      const resul = await BlockchainManager.getDidContract(options,Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[1].registry,web3);
+      expect(resul._address).toBe(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[1].registry);
+    });
+
+    test("Get rinkeby ethr-did-registry address", async() => {
+      const web3 = new Web3(new Web3.providers.HttpProvider(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[2].rpcUrl));
+      const resul = await BlockchainManager.getDidContract(options,Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[2].registry,web3);
+      expect(resul._address).toBe(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[2].registry);
+    });
+
+    test("Get goerli ethr-did-registry address", async() => {
+      const web3 = new Web3(new Web3.providers.HttpProvider(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[3].rpcUrl));
+      const resul = await BlockchainManager.getDidContract(options,Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[3].registry,web3);
+      expect(resul._address).toBe(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[3].registry);
+    });
+
+    test("Get kovan ethr-did-registry address", async() => {
+      const web3 = new Web3(new Web3.providers.HttpProvider(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[4].rpcUrl));
+      const resul = await BlockchainManager.getDidContract(options,Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[4].registry,web3);
+      expect(resul._address).toBe(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[4].registry);
+    });
+    
   });
 
   describe("getDidAddress", () => {
