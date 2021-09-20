8baee844aeb036397511fd4652b460d52e762562
Ese <silviaox@gmail.com>
2021-09-20 20:58:14 -0300

    fix const name resul 522

--- __tests__/didExtract.test.js
+++ __tests__/didExtract.test.js
@@ -13,69 +13,69 @@ describe("BlockchainManager did getters", () => {
   describe("getDidContract", () => {
     test("Get Lacchain ethr-did-registry address", async() => {
       const web3 = new Web3(new Web3.providers.HttpProvider(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[5].rpcUrl));
-      const resul = await BlockchainManager.getDidContract(options,Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[5].registry,web3);
-      expect(resul._address).toBe(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[5].registry);
+      const result = await BlockchainManager.getDidContract(options,Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[5].registry,web3);
+      expect(result._address).toBe(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[5].registry);
     });
 
     test("Get BFA ethr-did-registry address", async() => {
       const web3 = new Web3(new Web3.providers.HttpProvider(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[6].rpcUrl));
-      const resul = await BlockchainManager.getDidContract(options,Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[6].registry,web3);
-      expect(resul._address.toLowerCase()).toBe(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[6].registry);
+      const result = await BlockchainManager.getDidContract(options,Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[6].registry,web3);
+      expect(result._address.toLowerCase()).toBe(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[6].registry);
     });
 
     test("Get mainnet ethr-did-registry address", async() => {
       const web3 = new Web3(new Web3.providers.HttpProvider(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[0].rpcUrl));
-      const resul = await BlockchainManager.getDidContract(options,Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[0].registry,web3);
-      expect(resul._address.toLowerCase()).toBe(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[0].registry);
+      const result = await BlockchainManager.getDidContract(options,Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[0].registry,web3);
+      expect(result._address.toLowerCase()).toBe(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[0].registry);
     });
 
     test("Get ropsten ethr-did-registry address", async() => {
       const web3 = new Web3(new Web3.providers.HttpProvider(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[1].rpcUrl));
-      const resul = await BlockchainManager.getDidContract(options,Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[1].registry,web3);
-      expect(resul._address.toLowerCase()).toBe(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[1].registry);
+      const result = await BlockchainManager.getDidContract(options,Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[1].registry,web3);
+      expect(result._address.toLowerCase()).toBe(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[1].registry);
     });
 
     test("Get rinkeby ethr-did-registry address", async() => {
       const web3 = new Web3(new Web3.providers.HttpProvider(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[2].rpcUrl));
-      const resul = await BlockchainManager.getDidContract(options,Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[2].registry,web3);
-      expect(resul._address.toLowerCase()).toBe(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[2].registry);
+      const result = await BlockchainManager.getDidContract(options,Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[2].registry,web3);
+      expect(result._address.toLowerCase()).toBe(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[2].registry);
     });
 
     test("Get goerli ethr-did-registry address", async() => {
       const web3 = new Web3(new Web3.providers.HttpProvider(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[3].rpcUrl));
-      const resul = await BlockchainManager.getDidContract(options,Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[3].registry,web3);
-      expect(resul._address.toLowerCase()).toBe(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[3].registry);
+      const result = await BlockchainManager.getDidContract(options,Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[3].registry,web3);
+      expect(result._address.toLowerCase()).toBe(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[3].registry);
     });
 
     test("Get kovan ethr-did-registry address", async() => {
       const web3 = new Web3(new Web3.providers.HttpProvider(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[4].rpcUrl));
-      const resul = await BlockchainManager.getDidContract(options,Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[4].registry,web3);
-      expect(resul._address.toLowerCase()).toBe(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[4].registry);
+      const result = await BlockchainManager.getDidContract(options,Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[4].registry,web3);
+      expect(result._address.toLowerCase()).toBe(Constants.BLOCKCHAIN.PROVIDER_CONFIG.networks[4].registry);
     });
     
   });
 
   describe("getDidAddress", () => {
     test("Get address from did with network", async() => {
-      const resul = await BlockchainManager.getDidAddress(DIDS.didWithNwtwork);
-      expect(resul).toBe(DIDS.did);
+      const result = await BlockchainManager.getDidAddress(DIDS.didWithNwtwork);
+      expect(result).toBe(DIDS.did);
     });
   
     test("Get address from did without network", async() => {
-      const resul = await BlockchainManager.getDidAddress(DIDS.didWithoutNetwork);
-      expect(resul).toBe(DIDS.did);
+      const result = await BlockchainManager.getDidAddress(DIDS.didWithoutNetwork);
+      expect(result).toBe(DIDS.did);
     });
   });
   
   describe("getDidBlockchain", () => {
     test("Get blockchain name from did with network", async() => {
-      const resul = await BlockchainManager.getDidBlockchain(DIDS.didWithNwtwork);
-      expect(resul).toBe(DIDS.network);
+      const result = await BlockchainManager.getDidBlockchain(DIDS.didWithNwtwork);
+      expect(result).toBe(DIDS.network);
     });
   
     test("Get blockchain name from did without network", async() => {
-      const resul = await BlockchainManager.getDidBlockchain(DIDS.didWithoutNetwork);
-      expect(resul).toBe(null);
+      const result = await BlockchainManager.getDidBlockchain(DIDS.didWithoutNetwork);
+      expect(result).toBe(null);
    });
   });
 });
