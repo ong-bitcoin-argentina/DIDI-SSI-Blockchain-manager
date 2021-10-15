e42dc4dc1863e476d4ceec83650931d02633a07d
Matias Oveja Smith <matias.ovejasmith@bitcoinargentina.org>
2021-10-14 21:12:25 -0300

    test: Add verification test. Keep it skipped

---
+++ __tests__/full.test.ts
@@ -0,0 +1,75 @@
+import { BlockchainManager, CredentialVerificationResponse } from "../src/BlockchainManager";
+
+
+const Constants = require("./constants/Constants");
+const { initializeBlockchainManager } = require("./utils/utils");
+
+const config = {
+  gasPrice: 10000,
+  providerConfig: Constants.BLOCKCHAIN.PROVIDER_CONFIG,
+};
+
+let blockchainManager: BlockchainManager;
+let jwt: string;
+let createdCredential;
+
+const subject = {
+  DatosPersonales: {
+    preview: {
+      fields: ["dni", "names", "lastNames", "nationality"],
+      type: 2,
+    },
+    category: "identity",
+    data: {
+      dni: 12345678,
+      names: "Homero",
+      lastNames: "Simpson",
+      nationality: "Argentina",
+    },
+  },
+};
+
+let aYearFromNow = new Date();
+aYearFromNow.setFullYear(aYearFromNow.getFullYear() + 1);
+
+async function createJWT(identity): Promise<object> {
+  const signer = blockchainManager.getSigner(identity.privateKey);
+  const payload = { name: "TEST" };
+  jwt = await blockchainManager.createJWT(identity.did, identity.privateKey, payload );
+  return { jwt, payload };
+}
+
+xdescribe("Delegate an issuer, sign a credential and verify the credential an issuer.", () => {
+
+  it("Delegate a new Issuer", async () => {
+    blockchainManager = initializeBlockchainManager(config);
+    const didiIdentity = {
+      did: process.env.DELEGATOR_DID,
+      privateKey: process.env.DELEGATOR_PRIV_KEY,
+    };
+    const issuerIdentity = blockchainManager.createIdentity();
+    const subjectIdentity = blockchainManager.createIdentity();
+
+    // Delegate new Issuer in every Blockchain.
+    const delegateResponse = await blockchainManager.addDelegate(didiIdentity, issuerIdentity.did, '8640000');
+    delegateResponse.forEach(({network, status}) => {
+      const expectedStatus = network === 'mainnet' || network === 'goerli' ? 'rejected' : 'fulfilled' ;
+      expect(status).toBe(expectedStatus)
+    });
+
+    // Delegated issuer creates a new credential
+    let credential: string = await blockchainManager.createCredential(
+      subjectIdentity.did,
+      subject,
+      aYearFromNow,
+      issuerIdentity.did,
+      issuerIdentity.privateKey
+    );
+    // console.log(credential);
+
+    // The credential is verfie and if a did is providad, delegation is verified too.
+    const verificationResponse: CredentialVerificationResponse = await blockchainManager.verifyCredential(credential, didiIdentity.did);
+    // console.log(verificationResponse);
+    expect(verificationResponse).toBe(true)
+  });
+});
